6f717ddf5e35342eb07ea8600be68e4e3522b3ea
teiyo <nic.hu.ax@gmail.com>
2026-04-07 14:52:10 +0200

    Redefined some data types

--- Mesos/src/main/java/it/polimi/ingsw/model/board/Deck.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/board/Deck.java
@@ -3,19 +3,18 @@ package it.polimi.ingsw.model.board;
 import it.polimi.ingsw.model.card.AbstractCard;
 import it.polimi.ingsw.model.card.building.AbstractBuilding;
 
-import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class Deck {
     final private int gameSize;
     private final Board board;
 
-    private final Stack<AbstractCard>[] charEventCards;
+    private final Stack<AbstractCard> charEventCards;
     private final Queue<AbstractBuilding> buildingCards;
 
-    private final int[] ageBuildingsCount;
-    private int currentAge = 1;
+    private final List<Integer> ageBuildingsCount;
+    private int currentAge = 0;
+
 
     public Deck(int gameSize, Board board) {
         this.gameSize = gameSize;
