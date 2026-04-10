8df287ecf3e9e313aea576b7e61f82807ac817b4
teiyo <nic.hu.ax@gmail.com>
2026-04-10 02:05:32 +0200

    Added new field to AbstractCard and getters/setters

--- Mesos/src/main/java/it/polimi/ingsw/model/card/AbstractCard.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/card/AbstractCard.java
@@ -3,11 +3,25 @@ package it.polimi.ingsw.model.card;
 import it.polimi.ingsw.model.board.Row;
 
 public abstract class AbstractCard {
-    protected final int era;
+    protected int era;
+    protected boolean isFinal;
 
     public AbstractCard(int era) {
         this.era = era;
     }
 
+    public abstract AbstractCard copy();
     public abstract void moveTo(Row row);
+
+    public void setEra(int era) {
+        this.era = era;
+    }
+
+    public int getEra() {
+        return era;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
 }
