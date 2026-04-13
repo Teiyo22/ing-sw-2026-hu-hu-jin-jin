9ca21ba82af9f0c968adfa58163b25dcfba67be1
teiyo <nic.hu.ax@gmail.com>
2026-04-14 00:11:45 +0200

    Fixed wrong class name

--- Mesos/src/main/java/it/polimi/ingsw/model/utils/ConfigLoader.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/utils/ConfigLoader.java
@@ -4,15 +4,13 @@ import it.polimi.ingsw.model.board.OfferTile;
 import it.polimi.ingsw.model.board.OrderSlot;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class LoadConfig {
+public class ConfigLoader {
     private final Gson gson= new Gson();
 
     /**It loads the offerTile from a json file
