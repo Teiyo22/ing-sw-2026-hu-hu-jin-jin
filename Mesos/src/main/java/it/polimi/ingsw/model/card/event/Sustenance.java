e9a612229f1fb8e8ff6946ff3133070176ccf864
teiyo <nic.hu.ax@gmail.com>
2026-04-14 04:04:01 +0200

    Sustenance refactoring

--- Mesos/src/main/java/it/polimi/ingsw/model/card/event/Sustenance.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/card/event/Sustenance.java
@@ -5,11 +5,9 @@ import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.card.AbstractCard;
 import it.polimi.ingsw.model.board.Row;
 import it.polimi.ingsw.model.player.Player;
-import it.polimi.ingsw.model.player.Tribe;
-import it.polimi.ingsw.model.board.Board;
-import it.polimi.ingsw.model.board.OrderSlot;
 
-import java.util.ArrayList;
+import java.util.List;
+
 
 public class Sustenance extends AbstractEvent {
     @Expose
@@ -41,22 +39,18 @@ public class Sustenance extends AbstractEvent {
      * If there is not enough food it subtracts pp.*/
     @Override
     public void onEvent(Game game) {
-        OrderSlot[] order = game.getBoard().getOrderTile();
+        List<Player> players = game.getPlayers();
 
-        for(int i=0; i<order.length; i++) {  //apply the effects for each player
-            Player player = order[i].getAssignedPlayer();
+        for(Player player: players) {  //apply the effects for each player
             //get the number of tribe members
-            int tribeSize = player.getTribe().getTribeSize();
-            tribeSize -= player.getTribe().getSustenanceDiscount();  //discount caused by collectors and buildings
-            if(tribeSize>0) {
-                //if there is not enough food to feed every member subtract pp for every remaining member
-                if (tribeSize > player.getFood()) {
-                    tribeSize -= player.getFood();  //calculate number of unfed members
-                    player.setFood(0);  //spend all the food
-                    player.addPP(-tribeSize * ppMultiplier);  //lose pp
-                } else {
-                    player.addFood(-tribeSize);  //otherwise just remove the needed amount of food, 1 per member
-                }
+            int foodCost = Math.max(player.getTribe().getTribeSize() - player.getTribe().getSustenanceDiscount(), 0);
+            int unfedCount =  foodCost - player.getFood();
+
+            if (unfedCount > 0) {
+                player.setFood(0);  //spend all the food
+                player.addPP(unfedCount * ppMultiplier);  //lose pp
+            } else {
+                player.addFood(-foodCost);  //otherwise just remove the needed amount of food, 1 per member
             }
         }
     }
