070df6d7cd3ba106d157c6d5b12d899ec57c3095
Nico <nickolojin@gmail.com>
2026-04-15 10:59:15 +0200

    Added some buildings test

---
+++ Mesos/src/test/java/it/polimi/ingsw/model/card/building/ExtraActionBuildingTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.model.card.building;
+
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.gameState.ExtraActionState;
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.PlayerConfig;
+import it.polimi.ingsw.model.player.Totem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExtraActionBuildingTest {
+    private ExtraActionBuilding extraActionBuilding;
+    private Player p;
+    private ExtraActionState extraActionState;
+
+    @BeforeEach
+    void setUp(){
+        extraActionBuilding = new ExtraActionBuilding("ExtraActionBuilding",1,false,2,3,new BuildingHandler());
+        p=new Player("Ciccio", Totem.BLACK);
+    }
+
+    @Test
+    void applyEffectTest(){
+        extraActionState= new ExtraActionState(new Game(PlayerConfig.TWO), new BuildingHandler());
+        extraActionBuilding.applyEffect(extraActionState);
+        assertEquals(p,extraActionState.getPlayer());
+    }
+}
