01892bc8c53beb707d207006802a1f80f50c8f7f
teiyo <nic.hu.ax@gmail.com>
2026-04-21 16:35:00 +0200

    Changed virtual server and client

--- Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualClient.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualClient.java
@@ -5,14 +5,15 @@ import it.polimi.ingsw.model.player.Totem;
 
 import java.rmi.Remote;
 import java.util.List;
+import java.util.Map;
 
 public abstract class VirtualClient implements Remote {
     protected int id;
 
-    abstract void setWaitingLobbies(String clientID, List<Lobby> lobbies);
-    abstract void showLobbyInfo(Lobby lobby);
-    abstract void setLobby(String lobbyID, String playerName, Totem totem);
-    abstract void removeFromLobby(String lobbyID);
-    abstract void showRank();
-    abstract void showLeaderboard();
+    abstract void setWaitingLobbies(int clientID, List<Lobby> lobbies);
+    abstract void showLobbyInfo(int clientID, Lobby lobby);
+    abstract void setLobby(int clientID, int lobbyID, String playerName, Totem totem);
+    abstract void removeFromLobby(int clientID, int lobbyID);
+    abstract void showRank(int clientID, Map<Integer, Integer> rankings);
+    abstract void showLeaderboard(int clientID, List<LeaderboardEntry> leaderboard);
 }
--- Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualServer.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualServer.java
@@ -7,11 +7,11 @@ import java.rmi.Remote;
 
 public abstract class VirtualServer implements Remote {
     abstract void addClient(VirtualClient client);
-    abstract void createLobby(String clientID, String lobbyName, int playerNum, String playerName, Totem totem);
-    abstract void joinLobby(String clientID, String lobbyID, String playerName, Totem totem);
-    abstract void leaveLobby(String clientID, String lobbyID);
-    abstract void startLobby(String lobbyID);
-    abstract void getWaitingLobbies(String clientID);
-    abstract void getLobbyInfo(String lobbyID, String clientID);
-    abstract void getRank(String lobbyID, String clientID);
+    abstract void createLobby(int clientID, int playerNum, String playerName, Totem totem);
+    abstract void joinLobby(int clientID, String lobbyID, String playerName, Totem totem);
+    abstract void leaveLobby(int clientID, String lobbyID);
+    abstract void startLobby(int clientID, String lobbyID);
+    abstract void getWaitingLobbies(int clientID);
+    abstract void getLobbyInfo(int clientID, String lobbyID);
+    abstract void getRank(int clientID, String lobbyID);
 }
