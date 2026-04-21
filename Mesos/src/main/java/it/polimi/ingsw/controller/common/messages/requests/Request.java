89354fc1c7d532030666f0505bcb6975ae646f23
teiyo <nic.hu.ax@gmail.com>
2026-04-21 18:14:29 +0200

    Changed clientID type from String to int

--- Mesos/src/main/java/it/polimi/ingsw/controller/common/messages/requests/Request.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/common/messages/requests/Request.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.controller.common.messages.requests;
 import it.polimi.ingsw.controller.server.ServerController;
 
 public abstract class Request {
-    private String clientID;
+    private int clientID;
 
     public abstract void receive(ServerController serverController);
 }
