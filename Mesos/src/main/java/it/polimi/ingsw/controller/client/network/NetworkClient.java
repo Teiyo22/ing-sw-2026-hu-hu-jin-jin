ac5f7a51fbd6266f1db500844a5db4c094390296
jennieh0503 <hujennyfer0503@gmail.com>
2026-04-22 21:27:16 +0200

    Implemented NetworkClient, ServerTCPInterface, and connection methods in ClientController

--- Mesos/src/main/java/it/polimi/ingsw/controller/client/network/NetworkClient.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/client/network/NetworkClient.java
@@ -1,31 +1,54 @@
 package it.polimi.ingsw.controller.client.network;
 
 import com.google.gson.*;
+import java.io.*;
+import java.net.UnknownHostException;
 import it.polimi.ingsw.controller.common.messages.requests.Request;
+import it.polimi.ingsw.controller.common.messages.responses.Response;
 
 import java.net.Socket;
 
 public class NetworkClient implements Runnable {
     private ServerTCPInterface server;
     private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
     private Gson gson;
 
     public NetworkClient(){
-
+        this.server = null;
+        this.socket = null;
+        this.input = null;
+        this.output = null;
+        this.gson = new Gson();
     }
 
     @Override
     public void run() {
-
+        String line;
+        try {
+            while ((line = input.readLine()) != null) {
+                Response response = gson.fromJson(line, Response.class);
+                server.handleMessage(response);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading message in TCP: " + e.getMessage());
+        }
     }
 
     public void sendMessage(Request request){
-
+        String msg = gson.toJson(request);
+        output.println(msg);
     }
 
-    public void connect(String ip, int tcpPort){
-
+    public void connect(String ip, int tcpPort) throws UnknownHostException, IOException {
+        this.socket = new Socket(ip, tcpPort);
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.output = new PrintWriter(socket.getOutputStream(), true);
+        new Thread(this).start();
     }
 
-
+    public void setServer(ServerTCPInterface server) {
+        this.server = server;
+    }
 }
--- Mesos/src/main/java/it/polimi/ingsw/controller/client/network/ServerTCPInterface.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/client/network/ServerTCPInterface.java
@@ -14,57 +14,69 @@ public class ServerTCPInterface extends VirtualServer {
     private ClientController clientController;
     private NetworkClient serverHandler;
 
+    public ServerTCPInterface(ClientController clientController,  NetworkClient serverHandler) {
+        this.clientController = clientController;
+        this.serverHandler = serverHandler;
+        serverHandler.setServer(this);
+    }
+
     public void handleMessage(Response response) {
-
+        response.receive(clientController);
     }
 
-    @Override
-    public void addClient(VirtualClient client) {
-
-    }
+    public void addClient(VirtualClient client) {}
 
     @Override
     public void createLobby(int clientID, int playerNum, String playerName, Totem totem) {
-
+        CreateLobbyRequest request = new CreateLobbyRequest(clientID, playerNum, playerName, totem);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void joinLobby(int clientID, int lobbyID, String playerName, Totem totem) {
-
+        JoinLobbyRequest request = new JoinLobbyRequest(clientID, lobbyID, playerName, totem);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void leaveLobby(int clientID, int lobbyID) {
-
+        LeaveLobbyRequest request = new LeaveLobbyRequest(clientID, lobbyID);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void startLobby(int clientID, int lobbyID) {
-
+        StartLobbyRequest request = new StartLobbyRequest(clientID, lobbyID);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void getWaitingLobbies(int clientID) {
-
+        WaitingLobbyRequest request = new WaitingLobbyRequest(clientID);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void getLobbyInfo(int clientID, int lobbyID) {
-
+        LobbyInfoRequest request = new LobbyInfoRequest(clientID, lobbyID);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void getRank(int clientID, int lobbyID) {
-
+        GetRankRequest request = new GetRankRequest(clientID, lobbyID);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void getLeaderboard(int clientID, int playerNum) {
-
+        GetLeaderboardRequest request = new GetLeaderboardRequest(clientID, playerNum);
+        serverHandler.sendMessage(request);
     }
 
     @Override
     public void requestPick(int clientID, int lobbyID, List<AbstractCard> topPicks, List<AbstractCard> bottomPicks) {
-
+        PickCardsRequest request = new PickCardsRequest(clientID, lobbyID, topPicks, bottomPicks);
+        serverHandler.sendMessage(request);
     }
 }
