93848617a9897d21c3f745b74f974db83ce777b3
jennieh0503 <hujennyfer0503@gmail.com>
2026-04-22 19:07:49 +0200

    Updated virtualClient and virtualServer due to UML changes, fix

--- Mesos/src/main/java/it/polimi/ingsw/controller/client/ClientController.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/client/ClientController.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.controller.client;
 
-import it.polimi.ingsw.controller.client.network.NetworkClient;
-import it.polimi.ingsw.controller.client.network.ServerTCPInterface;
 import it.polimi.ingsw.controller.common.LeaderboardEntry;
 import it.polimi.ingsw.controller.common.Lobby;
 import it.polimi.ingsw.controller.common.VirtualClient;
@@ -9,8 +7,6 @@ import it.polimi.ingsw.controller.common.VirtualServer;
 import it.polimi.ingsw.model.card.AbstractCard;
 import it.polimi.ingsw.model.player.Totem;
 
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Map;
 
@@ -18,7 +14,7 @@ public class ClientController extends VirtualClient{
     private VirtualServer server;
 
     public ClientController() {
-        this.server = null;
+
     }
 
     @Override
@@ -60,11 +56,7 @@ public class ClientController extends VirtualClient{
 
     }
 
-    public void connectTCP(String ip, int tcpPort) throws UnknownHostException, IOException {
-        NetworkClient networkClient = new NetworkClient();
-        networkClient.connect(ip, tcpPort);
-        ServerTCPInterface serverTCPInterface= new ServerTCPInterface(this, networkClient);
-        networkClient.setServer(serverTCPInterface);
-        this.server = serverTCPInterface;
+    public void connectTCP(String ip, int tcpPort){
+
     }
 }
