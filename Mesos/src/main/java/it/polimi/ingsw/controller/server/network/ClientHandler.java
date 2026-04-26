e43cb955ec1c2332c4c9995031b4ad988920bdd0
Enrico <enrico.jin@mail.polimi.it>
2026-04-26 20:20:34 +0200

    Implemented ClientHandler

--- Mesos/src/main/java/it/polimi/ingsw/controller/server/network/ClientHandler.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/server/network/ClientHandler.java
@@ -1,18 +1,41 @@
 package it.polimi.ingsw.controller.server.network;
 
 import com.google.gson.Gson;
+import it.polimi.ingsw.controller.common.messages.requests.Request;
 import it.polimi.ingsw.controller.common.messages.responses.Response;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler extends Thread {
     private ClientTCPInterface clientInterface;
     private Socket socket;
     private Gson gson;
+    private BufferedReader input;
+    private PrintWriter output;
 
+    @Override
     public void run() {
+        String line;
+
+        try {
+            while ((line = input.readLine()) != null) {
+                Request request = gson.fromJson(line, Request.class);
+                clientInterface.handleMessage(request);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading message in TCP: " + e.getMessage());
+        }
     }
 
     public void sendMessage(Response response) {
+        String message = gson.toJson(response);
+        output.println(message);
+    }
+
+    public void setClientTCPInterface(ClientTCPInterface clientInterface){
+        this.clientInterface = clientInterface;
     }
 }
