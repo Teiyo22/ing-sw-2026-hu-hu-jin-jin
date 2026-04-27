c4f16f34404f9f78ae3ec9c236ae19dca481737a
teiyo <nic.hu.ax@gmail.com>
2026-04-27 12:29:39 +0200

    Removed player count from deckConfig

--- Mesos/src/main/java/it/polimi/ingsw/utils/DeckConfig.java
+++ Mesos/src/main/java/it/polimi/ingsw/utils/DeckConfig.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.utils;
 import java.util.List;
 
 public class DeckConfig {
-    private int playerCount;
     private List<Integer> buildingsCountPerAge;
     private List<CardConfig> buildingConfigs;
     private List<CardConfig> cardConfigs;
