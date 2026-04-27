0503d7b6061dce9bd781504a97bdfd8d632c4155
teiyo <nic.hu.ax@gmail.com>
2026-04-27 12:28:10 +0200

    Created config files and moved them to resources

--- Mesos/src/main/java/it/polimi/ingsw/model/player/PlayerConfig.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/player/PlayerConfig.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.model.player;
 
 public enum PlayerConfig {
-    TWO(2, "tmp.json", "tmp.json", "tmp.json"),
-    THREE(3, "tmp.json", "tmp.json", "tmp.json"),
-    FOUR(4, "tmp.json", "tmp.json", "tmp.json") ,
-    FIVE(5, "tmp.json", "tmp.json", "tmp.json");
+    TWO(2, "/configs/deck/two.json", "/configs/offerTrack/two.json", "/configs/orderTile/two.json"),
+    THREE(3, "/configs/deck/three.json", "/configs/offerTrack/three.json", "/configs/orderTile/three.json"),
+    FOUR(4, "/configs/deck/four.json", "/configs/offerTrack/four.json", "/configs/orderTile/four.json") ,
+    FIVE(5, "/configs/deck/five.json", "/configs/offerTrack/five.json", "/configs/orderTile/five.json");
 
     private final int n;
     private final String deckConfigFile;
