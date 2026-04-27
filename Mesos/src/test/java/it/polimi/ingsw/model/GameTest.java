ce5a35c0260826812e2ce097df0885d73e405717
teiyo <nic.hu.ax@gmail.com>
2026-04-27 15:58:40 +0200

    Game.java test

--- Mesos/src/test/java/it/polimi/ingsw/model/GameTest.java
+++ Mesos/src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.board.OfferTile;
+import it.polimi.ingsw.model.card.Pickable;
+import it.polimi.ingsw.model.card.character.AbstractCharacter;
+import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.PlayerConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,26 +13,57 @@ import it.polimi.ingsw.model.gameState.*;
 import it.polimi.ingsw.model.player.Totem;
 import it.polimi.ingsw.model.card.building.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameTest {
     private Game g;
+    private List<Player> players;
 
     @BeforeEach
     void setUp() {
-        g= new Game(PlayerConfig.TWO);
+        players = new ArrayList<>();
+        players.add(new Player("Ciccio", Totem.BLACK));
+        players.add(new Player("Gigio", Totem.WHITE));
+        players.add(new Player("Alice", Totem.WHITE));
+        players.add(new Player("Bob", Totem.WHITE));
+        players.add(new Player("Carlo", Totem.WHITE));
+        g = new Game(PlayerConfig.FIVE, players);
     }
 
     @Test
     void setGameStateTest() {
         BuildingHandler buildingHandler= new BuildingHandler();
-        GameState state= new GameStartState(g, buildingHandler);
+        GameState state = new GameStartState(g, buildingHandler);
         g.setGameState(state);
         assertEquals(GameStartState.class, g.getGameState().getClass());
     }
 
     @Test
-    void addPlayerTest() {
-        g.addPlayer(Totem.BLUE, "Gigio");
-        assertEquals(1,g.getPlayers().size());
+    void pickTest() {
+        List<Pickable> topPicks = new ArrayList<>();
+        List<Pickable> bottomPicks = new ArrayList<>();
+
+        int topRowInitialSize = g.getBoard().getTopRow().getCharacterCards().size();
+        int bottomRowInitialSize = g.getBoard().getBottomRow().getCharacterCards().size();
+        AbstractCharacter topCard = g.getBoard().getTopRow().getCharacterCards().get(0);
+        AbstractCharacter bottomCard = g.getBoard().getBottomRow().getCharacterCards().get(0);
+
+        topPicks.add(topCard);
+        bottomPicks.add(bottomCard);
+
+        g.pick(players.get(0), topPicks, bottomPicks);
+        assertEquals(topRowInitialSize - 1, g.getBoard().getTopRow().getCharacterCards().size());
+        assertEquals(bottomRowInitialSize - 1, g.getBoard().getBottomRow().getCharacterCards().size());
+        assertEquals(2, players.get(0).getTribe().getTribeSize());
+    }
+
+    @Test
+    void assignToTest() {
+        OfferTile offerTile = g.getBoard().getOfferTrack()[0];
+        g.assignTo(players.get(0), offerTile);
+
+        assertEquals(players.get(0), offerTile.getAssignedPlayer());
     }
 }
 
