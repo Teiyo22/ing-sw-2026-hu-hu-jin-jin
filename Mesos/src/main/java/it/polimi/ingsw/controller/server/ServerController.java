d6a6e9186497fc6522abca54202afe86ef18805f
teiyo <nic.hu.ax@gmail.com>
2026-04-27 11:50:09 +0200

    Method definition changes in ServerController

--- Mesos/src/main/java/it/polimi/ingsw/controller/server/ServerController.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/server/ServerController.java
@@ -3,8 +3,10 @@ package it.polimi.ingsw.controller.server;
 import it.polimi.ingsw.controller.common.VirtualClient;
 import it.polimi.ingsw.controller.common.VirtualServer;
 import it.polimi.ingsw.controller.server.network.NetworkServer;
+import it.polimi.ingsw.model.card.AbstractCard;
 import it.polimi.ingsw.model.player.Totem;
 
+import java.util.List;
 import java.util.Map;
 
 public class ServerController extends VirtualServer {
@@ -30,17 +32,17 @@ public class ServerController extends VirtualServer {
     }
 
     @Override
-    public void joinLobby(int clientID, String lobbyID, String playerName, Totem totem) {
+    public void joinLobby(int clientID, int lobbyID, String playerName, Totem totem) {
 
     }
 
     @Override
-    public void leaveLobby(int clientID, String lobbyID) {
+    public void leaveLobby(int clientID, int lobbyID) {
 
     }
 
     @Override
-    public void startLobby(int clientID, String lobbyID) {
+    public void startLobby(int clientID, int lobbyID) {
 
     }
 
@@ -50,12 +52,22 @@ public class ServerController extends VirtualServer {
     }
 
     @Override
-    public void getLobbyInfo(int clientID, String lobbyID) {
+    public void getLobbyInfo(int clientID, int lobbyID) {
 
     }
 
     @Override
-    public void getRank(int clientID, String lobbyID) {
+    public void getRank(int clientID, int lobbyID) {
+
+    }
+
+    @Override
+    public void getLeaderboard(int clientID, int playerNum) {
+
+    }
+
+    @Override
+    public void requestPick(int clientID, int lobbyID, List<AbstractCard> topPicks, List<AbstractCard> bottomPicks) {
 
     }
 }
