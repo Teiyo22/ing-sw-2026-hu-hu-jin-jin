3e9e345ca6099fc5ab83eb6e1431dee9e712879b
teiyo <nic.hu.ax@gmail.com>
2026-04-27 14:21:09 +0200

    Filled deck config file

--- Mesos/src/main/java/it/polimi/ingsw/model/player/PlayerConfig.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/player/PlayerConfig.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.model.player;
 
 public enum PlayerConfig {
-    TWO(2, "/configs/deck/deck.json", "/configs/offerTrack/two.json", "/configs/orderTile/two.json"),
-    THREE(3, "/configs/deck/deck.json", "/configs/offerTrack/three.json", "/configs/orderTile/three.json"),
-    FOUR(4, "/configs/deck/deck.json", "/configs/offerTrack/four.json", "/configs/orderTile/four.json") ,
-    FIVE(5, "/configs/deck/deck.json", "/configs/offerTrack/five.json", "/configs/orderTile/five.json");
+    TWO(2, "/configs/deck.json", "/configs/offerTrack/two.json", "/configs/orderTile/two.json"),
+    THREE(3, "/configs/deck.json", "/configs/offerTrack/three.json", "/configs/orderTile/three.json"),
+    FOUR(4, "/configs/deck.json", "/configs/offerTrack/four.json", "/configs/orderTile/four.json") ,
+    FIVE(5, "/configs/deck.json", "/configs/offerTrack/five.json", "/configs/orderTile/five.json");
 
     private final int n;
     private final String deckConfigFile;
