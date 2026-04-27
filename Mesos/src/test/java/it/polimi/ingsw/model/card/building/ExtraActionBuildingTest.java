07fd24ee32bba65c745e1150a8b4eea8a4975631
teiyo <nic.hu.ax@gmail.com>
2026-04-27 11:24:56 +0200

    Small fix in test

--- Mesos/src/test/java/it/polimi/ingsw/model/card/building/ExtraActionBuildingTest.java
+++ Mesos/src/test/java/it/polimi/ingsw/model/card/building/ExtraActionBuildingTest.java
@@ -8,23 +8,30 @@ import it.polimi.ingsw.model.player.Totem;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExtraActionBuildingTest {
     private ExtraActionBuilding extraActionBuilding;
-    private Player p;
+    private List<Player> players;
     private ExtraActionState extraActionState;
 
     @BeforeEach
     void setUp(){
         extraActionBuilding = new ExtraActionBuilding("ExtraActionBuilding",1,false,2,3);
-        p=new Player("Ciccio", Totem.BLACK);
+        players = new ArrayList<>();
+        players.add(new Player("Ciccio", Totem.BLACK));
+        players.add(new Player("Gigio", Totem.WHITE));
     }
 
     @Test
     void applyEffectTest(){
-        extraActionState= new ExtraActionState(new Game(PlayerConfig.TWO), new BuildingHandler());
+        extraActionState= new ExtraActionState(new Game(PlayerConfig.TWO, players), new BuildingHandler());
+        extraActionBuilding.onPick(players.get(0), new BuildingHandler());
         extraActionBuilding.applyEffect(extraActionState);
-        assertEquals(p,extraActionState.getCurrPlayer());
+        assertEquals(players.get(0),extraActionState.getCurrPlayer());
     }
 }
