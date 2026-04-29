a89bd9d4b7b119bb3e0ed81197a4fc14ca9d2f3e
jennieh0503 <hujennyfer0503@gmail.com>
2026-04-29 11:05:04 +0200

    Implemented connectionState, updated TUIView

--- Mesos/src/main/java/it/polimi/ingsw/controller/client/view/TUI/TUIView.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/client/view/TUI/TUIView.java
@@ -8,12 +8,18 @@ import it.polimi.ingsw.controller.client.view.VirtualView;
 import java.util.Scanner;
 
 public class TUIView extends VirtualView implements Runnable {
+    private final String registryName;
+    private final String ip;
+    private final int port;
     private ViewState state;
     private final Scanner scanner;
 
-    public TUIView(ClientController clientController) {
+    public TUIView(ClientController clientController, String registryName, String ip, int port) {
         super(clientController);
-        state = new ConnectionState(clientController);
+        this.registryName = registryName;
+        this.ip = ip;
+        this.port = port;
+        state = new ConnectionState(clientController, registryName, ip, port);
         scanner = new Scanner(System.in);
     }
 
@@ -25,7 +31,7 @@ public class TUIView extends VirtualView implements Runnable {
     @Override
     public void transitionTo(ViewStates newState) {
         switch (newState) {
-            case CONNECTION -> state = new ConnectionState(super.getClientController());
+            case CONNECTION -> state = new ConnectionState(super.getClientController(), registryName, ip, port);
             case LOBBY_SELECTION ->  state = new LobbySelectionState(super.getClientController());
             case LOBBY_WAITING ->   state = new LobbyWaitingState(super.getClientController());
             case ROUND_START ->   state = new RoundStartState(super.getClientController());
--- Mesos/src/main/java/it/polimi/ingsw/controller/client/view/TUI/states/ConnectionState.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/client/view/TUI/states/ConnectionState.java
@@ -3,17 +3,30 @@ package it.polimi.ingsw.controller.client.view.TUI.states;
 import it.polimi.ingsw.controller.client.ClientController;
 
 public class ConnectionState extends ViewState {
-    public ConnectionState(ClientController controller) {
+    private final String registryName;
+    private final String ip;
+    private final int port;
+
+    public ConnectionState(ClientController controller, String registryName, String ip, int port) {
         super(controller);
+        this.registryName = registryName;
+        this.ip = ip;
+        this.port = port;
     }
 
     @Override
     public void render() {
-
+        System.out.println("Select connection mode:");
+        System.out.println("1. RMI");
+        System.out.println("2. TCP");
     }
 
     @Override
     public void handleInput(String input) {
-
+        switch (input) {
+            case "1": super.getController().connectRMI(registryName, ip, port);
+            case "2": super.getController().connectTCP(ip, port);
+            default: System.out.println("Invalid input: select 1 or 2.");
+        }
     }
 }
