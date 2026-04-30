1e0ce4d3e34bfa990f8999879988e0a0df7447f5
teiyo <nic.hu.ax@gmail.com>
2026-04-30 16:31:17 +0200

    VirtualClient methods throw RemoteException for RMI disconnection

--- Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualClient.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/common/VirtualClient.java
@@ -7,28 +7,29 @@ import it.polimi.ingsw.model.card.AbstractCard;
 import it.polimi.ingsw.model.player.Tribe;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
 
 public abstract class VirtualClient implements Remote {
     protected int id;
 
-    public void setID(int clientID){
+    public void setID(int clientID) throws RemoteException {
         this.id = clientID;
     }
-    public int getID(){
+    public int getID() throws RemoteException {
         return id;
     }
-    public abstract void setWaitingLobbies(int clientID, List<Lobby> lobbies);
-    public abstract void showLobbyInfo(int clientID, int lobbyID, Map<Integer, Player> players);
-    public abstract void setLobby(int clientID, int lobbyID, Player player);
-    public abstract void removeFromLobby(int clientID, int lobbyID);
-    public abstract void showRank(int clientID, int lobbyID, Map<Integer, Integer> rankings);
-    public abstract void showLeaderboard(int clientID, List<LeaderboardEntry> leaderboard);
-    public abstract void confirmPick(int clientID, Board board, Tribe updatedTribe);
-    public abstract void createLobby(int clientID, Lobby lobby, Player player);
-    public abstract void startLobby(int clientID, int lobbyID, Board board, Map<Integer, Tribe> tribes);
-    public abstract void terminateLobby(int lobbyID);
+    public abstract void setWaitingLobbies(int clientID, List<Lobby> lobbies) throws RemoteException;
+    public abstract void showLobbyInfo(int clientID, int lobbyID, Map<Integer, Player> players) throws RemoteException;
+    public abstract void setLobby(int clientID, int lobbyID, Player player) throws RemoteException;
+    public abstract void removeFromLobby(int clientID, int lobbyID) throws RemoteException;
+    public abstract void showRank(int clientID, int lobbyID, Map<Integer, Integer> rankings) throws RemoteException;
+    public abstract void showLeaderboard(int clientID, List<LeaderboardEntry> leaderboard) throws RemoteException;
+    public abstract void confirmPick(int clientID, Board board, Tribe updatedTribe) throws RemoteException;
+    public abstract void createLobby(int clientID, Lobby lobby, Player player) throws RemoteException;
+    public abstract void startLobby(int clientID, int lobbyID, Board board, Map<Integer, Tribe> tribes) throws RemoteException;
+    public abstract void terminateLobby(int lobbyID) throws RemoteException;
     public abstract void deleteLobby(int lobbyID) throws RemoteException;
     public abstract void ping() throws Exception;
 }
