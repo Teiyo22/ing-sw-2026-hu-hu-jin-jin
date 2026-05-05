b4e94f783dec5dca2ea01c72f30e67bf49a6f73c
teiyo <nic.hu.ax@gmail.com>
2026-05-05 13:19:02 +0200

    Modified RMI main for testing

--- Mesos/src/main/java/it/polimi/ingsw/ClientMainRMI.java
+++ Mesos/src/main/java/it/polimi/ingsw/ClientMainRMI.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.controller.client.ClientController;
 import it.polimi.ingsw.utils.Logger;
 import it.polimi.ingsw.utils.LoggerLevel;
+import it.polimi.ingsw.view.View;
+import it.polimi.ingsw.view.ViewFactory;
 
 public class ClientMainRMI {
     public static void main(String[] args) {
@@ -12,7 +14,7 @@ public class ClientMainRMI {
 //        }
 
         Logger l = Logger.getInstance();
-        l.setLevel(LoggerLevel.MODEL);
+        l.setLevel(LoggerLevel.OFF);
 
         String address = "127.0.0.1"; // args[0];
         int tcpPort = 28910; // args.length == 3 ? Integer.parseInt(args[1]) : 0;
@@ -26,12 +28,10 @@ public class ClientMainRMI {
         ClientController controller = new ClientController();
         controller.connectRMI(address, rmiPort);
 
-        String line = null;
-        do {
-            line = System.console().readLine();
-        } while (!line.trim().equalsIgnoreCase("stop"));
+        View view = ViewFactory.create("tui", controller);
+        controller.setView(view);
 
-        controller.disconnect();
+        view.show();
 
         System.exit(0);
     }
