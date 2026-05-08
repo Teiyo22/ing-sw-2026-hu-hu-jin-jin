dc1fe7014000e08bd4fde76ea67fe19b15709405
jennieh0503 <hujennyfer0503@gmail.com>
2026-05-08 16:11:23 +0200

    Updated abstract GUIScreen

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIScreen.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIScreen.java
@@ -1,20 +1,22 @@
 package it.polimi.ingsw.view.gui.screen;
 
+import it.polimi.ingsw.controller.client.ClientController;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class GUIScreen implements ActionListener {
     JFrame frame;
+    ClientController controller;
 
-    public GUIScreen(JFrame frame) {
+    public GUIScreen(JFrame frame, ClientController controller) {
         this.frame = frame;
+        this.controller = controller;
     }
 
     public abstract void render();
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
+    public void actionPerformed(ActionEvent e){}
 }
