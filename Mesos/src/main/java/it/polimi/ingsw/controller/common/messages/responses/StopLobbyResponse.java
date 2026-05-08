71f4cb423d6b19f2a205968c762d188c6fec9fa0
teiyo <nic.hu.ax@gmail.com>
2026-05-08 19:48:22 +0200

    Added missing statements in StopLobbyResponse

--- Mesos/src/main/java/it/polimi/ingsw/controller/common/messages/responses/StopLobbyResponse.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/common/messages/responses/StopLobbyResponse.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.controller.common.messages.responses;
 
 import it.polimi.ingsw.controller.client.ClientController;
+import it.polimi.ingsw.controller.common.messages.MessageType;
 import it.polimi.ingsw.controller.common.messages.Response;
 
 public class StopLobbyResponse extends Response {
@@ -8,11 +9,12 @@ public class StopLobbyResponse extends Response {
 
     public StopLobbyResponse(int clientID, int lobbyID){
         super(clientID);
+        this.type = MessageType.STOP_LOBBY;
         this.lobbyID = lobbyID;
     }
 
     @Override
     public void receive(ClientController clientController) {
-
+        clientController.stopLobby(clientID, lobbyID);
     }
 }
