296fe0926e209f99a2ef26c26ba9466ac3d0268f
jennieh0503 <hujennyfer0503@gmail.com>
2026-05-09 20:08:44 +0200

    Small fix

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIScreen.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIScreen.java
@@ -7,8 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class GUIScreen implements ActionListener {
-    JFrame frame;
-    ClientController clientController;
+    protected JFrame frame;
+    protected ClientController clientController;
 
     public GUIScreen(JFrame frame, ClientController clientController) {
         this.frame = frame;
