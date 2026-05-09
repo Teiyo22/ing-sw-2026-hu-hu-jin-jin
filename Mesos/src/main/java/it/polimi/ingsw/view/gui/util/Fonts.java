46856e9ce020a3226d26497427ddc4d2481bf0a2
Nico <binghaonicolo.jin@mail.polimi.it>
2026-05-09 18:52:21 +0200

    Added Fonts

---
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.view.gui.util;
+
+import java.awt.*;
+
+public class Fonts {
+    public static final Font large  = new Font("Arial", Font.PLAIN, 40);
+    public static final Font medium = new Font("Arial", Font.PLAIN, 30);
+    public static final Font small  = new Font("Arial", Font.PLAIN, 20);
+
+    public static final Color black   = new Color(0x000000);
+}
