6c3fb41454390f2e5e1dcfa9157e9fe40b1157d4
jennieh0503 <hujennyfer0503@gmail.com>
2026-05-10 16:52:09 +0200

    Added lobby selection case to ScreenType method

--- Mesos/src/main/java/it/polimi/ingsw/view/ScreenType.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/ScreenType.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.controller.client.ClientController;
 import it.polimi.ingsw.view.gui.screen.GUIScreen;
+import it.polimi.ingsw.view.gui.screen.WaitingLobbiesScreen;
 import it.polimi.ingsw.view.tui.screen.*;
 
 import javax.swing.*;
@@ -19,6 +20,7 @@ public enum ScreenType {
 
     public static GUIScreen getGUIScreen(ScreenType type, JFrame frame, ClientController clientController) {
         return switch(type) {
+            case LOBBY_SELECTION -> new WaitingLobbiesScreen(frame, clientController);
             case GAME_PLAY -> new it.polimi.ingsw.view.gui.screen.GamePlayScreen(frame, clientController);
         };
     }
