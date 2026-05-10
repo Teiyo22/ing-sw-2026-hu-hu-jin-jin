c7352cca10735a0172c24535df1c24cfd7819aa5
Nico <binghaonicolo.jin@mail.polimi.it>
2026-05-10 22:46:28 +0200

    Added elements

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
@@ -8,6 +8,7 @@ public class Fonts {
     public static final Font small  = new Font("Arial", Font.PLAIN, 20);
 
     public static final Color black   = new Color(0x000000);
+    public static final Color grey = new Color(30, 30, 30);
     public static final Color red      = new Color(0xEE3F2A);
     public static final Color parchment   = new Color(0xFFF3D3);
     public static final Color gold       = new Color(255, 215,   0);
