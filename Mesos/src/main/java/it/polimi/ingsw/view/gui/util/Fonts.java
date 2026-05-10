9faf9eb11a8dc9e5c0b47bb30c7472c04401e278
Enrico <enrico.jin@mail.polimi.it>
2026-05-10 20:01:31 +0200

    started GameEndScreen implementation

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
@@ -8,4 +8,9 @@ public class Fonts {
     public static final Font small  = new Font("Arial", Font.PLAIN, 20);
 
     public static final Color black   = new Color(0x000000);
+    public static final Color red      = new Color(0xEE3F2A);
+    public static final Color parchment   = new Color(0xFFF3D3);
+    public static final Color gold       = new Color(255, 215,   0);
+    public static final Color silver      = new Color(211, 211, 211);
+    public static final Color bronze = new Color(205, 127,  50);
 }
