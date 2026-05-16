75c08915a8acd455dcb5d31363dda2dd390e247c
Enrico <enrico.jin@mail.polimi.it>
2026-05-16 12:57:29 +0200

    started tabs implementation

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIGamePlayScreen.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/screen/GUIGamePlayScreen.java
@@ -31,7 +31,8 @@ public class GUIGamePlayScreen extends GUIScreen{
         sections = List.of(new TopRowSection(topListener),
                 new BottomRowSection(bottomListener),
                 new OfferTrackSection(controller, offerPickListener),
-                new GameInfoSection(clientController, topListener, bottomListener, offerPickListener));
+                new GameInfoSection(clientController, topListener, bottomListener, offerPickListener),
+                new TribesSection(clientController));
 
         topRowPanel = sections.get(0).getPanel();
         bottomRowPanel = sections.get(1).getPanel();
@@ -39,12 +40,40 @@ public class GUIGamePlayScreen extends GUIScreen{
         gameInfoPanel = sections.get(3).getPanel();
 
         JPanel board = new PanelBuilder().border(topRowPanel, offerTrackPanel, bottomRowPanel, null, null).buildPanel();
+        JPanel infoPanel = new PanelBuilder().column(0, gameInfoPanel).withPadding(30, 30, 30, 0).buildPanel();
+
+        board.setOpaque(false);
+        infoPanel.setOpaque(false);
+
+        JPanel container = new JPanel(new BorderLayout());
+        container.add(infoPanel, BorderLayout.WEST);
+        container.add(board, BorderLayout.CENTER);
+        container.setOpaque(false);
+
+
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.setBackground(Color.BLACK);
+        tabs.setForeground(Color.WHITE);
+        tabs.setOpaque(false);
+        tabs.setUI(new javax.swing.plaf.basic.BasicTabbedPaneUI() {
+            @Override
+            protected void paintContentBorder(Graphics g, int tabPlacement, int selectedIndex) {
+            }
+
+            @Override
+            protected void paintTabBackground(Graphics g, int tabPane, int itemIndex, int x, int y, int w, int h, boolean isSelected) {
+                g.setColor(isSelected ? Color.BLACK : Color.DARK_GRAY);
+                g.fillRect(x, y, w, h);
+            }
+        });
+
+        tabs.addTab("Game", container);
+
+        JPanel tribesPanel = sections.get(4).getPanel();
+        tabs.addTab("Tribes", tribesPanel);
 
         this.setLayout(new BorderLayout());
-        this.add(new PanelBuilder().column(0, gameInfoPanel).withPadding(30, 30, 30, 0).buildPanel()
-                , BorderLayout.WEST);
-        this.add(board, BorderLayout.CENTER);
-
+        this.add(tabs, BorderLayout.CENTER);
     }
 
     @Override
