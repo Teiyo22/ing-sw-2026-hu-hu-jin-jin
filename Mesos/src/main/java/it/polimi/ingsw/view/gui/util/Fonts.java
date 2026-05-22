21b63133bb4568be3b9886c371a46099a2add5fc
teiyo <nic.hu.ax@gmail.com>
2026-05-22 16:12:30 +0200

    Revert "Changed to SimSun"
    This reverts commit 6c3409ee0cc24e6c522e61770e99592c800dddb0.

--- Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/gui/util/Fonts.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.view.gui.util;
 import java.awt.*;
 
 public class Fonts {
-    public static final Font large  = new Font("SimSun", Font.PLAIN, 40);
-    public static final Font medium = new Font("SimSun", Font.PLAIN, 30);
-    public static final Font small  = new Font("SimSun", Font.PLAIN, 20);
-    public static final Font tiny  = new Font("SimSun", Font.PLAIN, 15);
+    public static final Font large  = new Font("MS Gothic", Font.PLAIN, 40);
+    public static final Font medium = new Font("MS Gothic", Font.PLAIN, 30);
+    public static final Font small  = new Font("MS Gothic", Font.PLAIN, 20);
+    public static final Font tiny  = new Font("MS Gothic", Font.PLAIN, 15);
     public static final Font monospaced = new Font(Font.MONOSPACED, Font.PLAIN, 15);
 
     //Mesos cover art palette
